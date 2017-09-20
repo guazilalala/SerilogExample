f6722ab69c13d104159ad3819d43326833246653
Feet <59450804@qq.com>
2017-09-21 00:09:21 +0800

    new

---
+++ SerilogExample/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SerilogExample.Models;
+using Microsoft.Extensions.Logging;
+
+namespace SerilogExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+		private readonly ILogger<HomeController> _logger;
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+		public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+			_logger.LogInformation("test");
+			ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SerilogExample/Serilog/SerilogSetup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerilogExample.Serilog
+{
+    public static class SerilogSetup
+    {
+		public static void ConfigureSerilogMSSQL(this IServiceCollection services, IConfiguration configuration)
+		{
+			var connectionString = @"Data Source=.;Initial Catalog=Serilog;Persist Security Info=True;User ID=sa;Password=infohand.com";  // or the name of a connection string in your .config file
+			var tableName = "Logs";
+			var columnOptions = new ColumnOptions();  // optional
+
+			var log = new LoggerConfiguration()
+				.WriteTo.MSSqlServer(connectionString, tableName, columnOptions: columnOptions)
+				.CreateLogger();
+		}
+
+		public static void ConfigureSerilogRollFile(this IServiceCollection services, IConfiguration configuration)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.WriteTo.RollingFile("Logs\\{Date}.xt")
+				.CreateLogger();
+		}
+	}
+}
